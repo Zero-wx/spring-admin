cc7d4dfe5b9ea8ebcf59dde6226586009d1393b5
huangweixiang <huangwx313522@163.com>
2022-11-01 13:29:48 +0800

    12

--- spring-admin-server/spring-admin-code/src/main/java/com/spring/adminApi/dao/BaseRepository.java
+++ spring-admin-server/spring-admin-code/src/main/java/com/spring/adminApi/dao/BaseRepository.java
@@ -4,6 +4,10 @@ package com.spring.adminApi.dao;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.springframework.stereotype.Repository;
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+
+>>>>>>> parent of f7c04a2 (service)
 =======
 
 >>>>>>> parent of f7c04a2 (service)
