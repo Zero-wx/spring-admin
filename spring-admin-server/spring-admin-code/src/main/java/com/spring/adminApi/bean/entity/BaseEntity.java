2006e816d114826069c91bfc8182f750d5ff7865
huangweixiang <huangwx313522@163.com>
2022-11-01 13:33:53 +0800

    修正

--- spring-admin-server/spring-admin-code/src/main/java/com/spring/adminApi/bean/entity/BaseEntity.java
+++ spring-admin-server/spring-admin-code/src/main/java/com/spring/adminApi/bean/entity/BaseEntity.java
@@ -2,9 +2,10 @@ package com.spring.adminApi.bean.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
-import lombok.Data;
+
 
 import java.io.Serializable;
+import java.util.Date;
 
 public abstract class BaseEntity implements Serializable {
     @TableId(type = IdType.AUTO)
--- spring-admin-server/spring-admin-code/src/main/java/com/spring/adminApi/dao/BaseRepository.java
+++ spring-admin-server/spring-admin-code/src/main/java/com/spring/adminApi/dao/BaseRepository.java
@@ -1,19 +1,6 @@
 package com.spring.adminApi.dao;
 
-
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.springframework.stereotype.Repository;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> parent of f7c04a2 (service)
-=======
-=======
->>>>>>> parent of f7c04a2 (service)
-
->>>>>>> parent of f7c04a2 (service)
 
 public interface BaseRepository<T> extends BaseMapper<T> {
 }
