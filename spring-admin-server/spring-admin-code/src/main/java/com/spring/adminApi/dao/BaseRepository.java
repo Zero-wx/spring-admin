8283cd9bd2f6f6a4f4bb6eaaa784082fc741274f
huangweixiang <huangwx313522@163.com>
2022-11-01 13:29:29 +0800

    11

--- spring-admin-server/spring-admin-code/src/main/java/com/spring/adminApi/dao/BaseRepository.java
+++ spring-admin-server/spring-admin-code/src/main/java/com/spring/adminApi/dao/BaseRepository.java
@@ -3,6 +3,10 @@ package com.spring.adminApi.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.springframework.stereotype.Repository;
+<<<<<<< HEAD
+=======
+
+>>>>>>> parent of f7c04a2 (service)
 
 public interface BaseRepository<T> extends BaseMapper<T> {
 }
